4af49cd56ff800020ecc58688e5f6407fcb9ff57
C-Hunter77 <knowledgeisking77@gmail.com>
2025-05-20 09:56:23 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,19 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-
-  const env = loadEnv(mode, process.cwd(), '');
-
-  for (const k of Object.keys(env)) {
-    process.env[k] = env[k];
-  }
+  // Load environment variables based on the current mode (development, production)
+  const env = loadEnv(mode, process.cwd());
 
   return {
     plugins: [react()],
+    base: './', // Needed for static deployment like Render
 
     define: {
-      'process.env': process.env
-    }
+      // This allows you to use process.env.VITE_... in your frontend code
+      'process.env': Object.entries(env).reduce((prev, [key, val]) => {
+        prev[key] = JSON.stringify(val);
+        return prev;
+      }, {} as Record<string, string>),
+    },
   };
 });
